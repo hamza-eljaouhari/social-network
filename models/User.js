565545acb1fdae1dc9c3fa6814021a732cc7a900
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2021-03-11 21:29:57 +0100

    Changed name field to username

--- models/User.js
+++ models/User.js
@@ -18,8 +18,8 @@ module.exports = (sequelize, DataTypes) => {
       let status = await bcrypt.compare(password,user.toJSON().password)
       if(!status) return {message : 'Wrong email or password'}
   
-      let token = jwt.sign({ id: user.id, name: user.username, email: user.email }, process.env.JWT_ENCRYPTION, { expiresIn: process.env.JWT_EXPIRATION });
-      return ({ user: { id: user.id, name: user.username, email: user.email }, access_token: token })
+      let token = jwt.sign({ id: user.id, username: user.username, email: user.email }, process.env.JWT_ENCRYPTION, { expiresIn: process.env.JWT_EXPIRATION });
+      return ({ user: { id: user.id, username: user.username, email: user.email }, access_token: token })
     };
     User.signUp = async (name,email,password) => {
       let user = await User.findOne({ where: { email: email } })
@@ -27,11 +27,11 @@ module.exports = (sequelize, DataTypes) => {
   
       const encryptedPass = await bcrypt.hash(password, parseInt(process.env.JWT_SALT))
       user = await User.create({ email: email, 
-                                  name: name, 
+                                  username: name, 
                               password: encryptedPass,
                               })
-      const token = jwt.sign({ id: user.id, name: user.username, email: user.email }, process.env.JWT_ENCRYPTION, { expiresIn: process.env.JWT_EXPIRATION });
-      return ({ user: { id: user.id, name: user.username, email: user.email }, access_token: token })
+      const token = jwt.sign({ id: user.id, username: user.username, email: user.email }, process.env.JWT_ENCRYPTION, { expiresIn: process.env.JWT_EXPIRATION });
+      return ({ user: { id: user.id, username: user.username, email: user.email }, access_token: token })
     }
 
   User.associate = (models) => {
