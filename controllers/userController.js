7939eaa9fe2ae749c576e3014dab71cf1f15e8c4
Hamza ELJAOUHARI <hamza.eljaouhari.1995@gmail.com>
2021-03-11 21:31:45 +0100

    Changed name to username on usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,17 +6,17 @@ module.exports.signUp = async (req,res,next) => {
     try{
         //validate fields
         const schema = {
-            name : Joi.string().min(2).max(100).required(),
+            username : Joi.string().min(2).max(100).required(),
             email: Joi.string().email().min(5).max(100).required(),
             password : Joi.string().min(5).max(100).required()
         };
         await Joi.validate(req.body, schema)
                 .catch(e => { throw new Error(e.details[0].message )})
 
-        let name  = req.body.name,
+        let username  = req.body.usernamename,
             email = req.body.email,
         password = req.body.password;
-        let response = await model.User.signUp(name,email,password)
+        let response = await model.User.signUp(usernname,email,password)
         return res.status(200).send(response)
     }catch(error){
         return res.status(400).send(error)
